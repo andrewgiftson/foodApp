5cc3478643f004a4ad3a2c1ae535a3837763168d
andrewgiftson <andrewgiftson@gmail.com>
2021-12-20 18:46:58 +0530

    commit message

---
+++ script/init_server.sh
@@ -0,0 +1,18 @@
+echo "> Check the currently running spring server pid "
+ CURRENT_PID=$(ps -ef | grep java | grep tomcat | awk '{print $2}')
+echo "$CURRENT_PID" 
+ if [ -z $CURRENT_PID ]; then
+echo "> No applications are currently running and will not be shut down." 
+else
+echo "> kill -9 $CURRENT_PID" 
+kill -9 $CURRENT_PID
+sleep 10
+fi
+echo  rm -rf /usr/share/tomcat9/armstrong /usr/share/tomcat9/armstrong.war
+ echo nohup mv /home/andrew/workspace/Online food order/dist/*.war /usr/share/tomcat9/armstrong.war >> 
+ mv /home/andrew/workspace/Online food order/dist/*.war /usr/share/tomcat9/armstrong.war
+ sleep 10
+ cd /usr/share/tomcat9/bin
+ ./startup.sh
+ echo "Execution complete."
+ 
