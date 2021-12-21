a17ad095181594a6258b4611b5ffc9ff734cf602
andrewgiftson <andrewgiftson@gmail.com>
2021-12-21 18:50:44 +0530

    message

--- script/init_server.sh
+++ script/init_server.sh
@@ -5,11 +5,11 @@ echo "$CURRENT_PID"
 echo "> No applications are currently running and will not be shut down." 
 else
 echo "> kill -9 $CURRENT_PID" 
-kill -9 $CURRENT_PID
+
 sleep 10
 fi
 rm -rf /usr/share/tomcat9/armstrong.ant.tear /usr/share/tomcat9/armstrong.ant.tear.war
-echo nohup mv /home/andrew/workspace/'Online food order'/dist/*.war /usr/share/tomcat9/armstrongant.ant.tear.war >> 
+
  mv /home/andrew/workspace/'Online food order'/dist/*.war /usr/share/tomcat9/armstrong.ant.tear.war
  sleep 10
  cd /usr/share/tomcat9/bin
