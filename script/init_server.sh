786f9b3e9c451ea4bbfdda7f5f57fc67ea3d3e2d
andrewgiftson <andrewgiftson@gmail.com>
2022-01-04 16:50:50 +0530

    message

--- script/init_server.sh
+++ script/init_server.sh
@@ -9,7 +9,9 @@ echo "> kill -9 $CURRENT_PID"
 sleep 10
 fi
 rm -rf //var/lib/tomcat9/webapps/ROOT/armstrong.ant.tear //var/lib/tomcat9/webapps/ROOT/armstrong.ant.tear.war
+echo "File is removed"
 mv /home/andrew/workspace/foodapp/dist/*.war  /var/lib/tomcat9/webapps/ROOT
+echo "file is moved"
  sleep 10
  cd //usr/share/tomcat9/bin
  ./startup.sh
