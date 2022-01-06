ca8427d862bb9510dd43d0ec5bb7335c031485a9
andrewgiftson <andrewgiftson@gmail.com>
2022-01-06 17:03:23 +0530

    message

--- script/init_server.sh
+++ script/init_server.sh
@@ -10,7 +10,7 @@ sleep 10
 fi
 rm -rf //var/lib/tomcat9/webapps/foodApp //var/lib/tomcat9/webapps/foodApp.war
 echo "File is removed //var/lib/tomcat9/webapps/foodApp //var/lib/tomcat9/webapps/foodApp.war"
-mv /home/andrew/workspace/foodapp/dist/*.war  /var/lib/tomcat9/webapps
+cp /home/andrew/workspace/foodapp/dist/*.war  /var/lib/tomcat9/webapps
 echo "file is moved /home/andrew/workspace/foodapp/dist/*.war  /var/lib/tomcat9/webapps"
  sleep 10
  cd //usr/share/tomcat9/bin
