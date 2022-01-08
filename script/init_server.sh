712a803dc9f26f9dcd1e5b58cbe715bb248fe27b
andrewgiftson <andrewgiftson@gmail.com>
2022-01-08 16:09:01 +0530

    message

--- script/init_server.sh
+++ script/init_server.sh
@@ -1,16 +1,8 @@
-echo "> Check the currently running spring server pid "
- CURRENT_PID=$(ps -ef | grep java | grep tomcat | awk '{print $2}')
-echo "$CURRENT_PID" 
- if [ -z $CURRENT_PID ]; then
-echo "> No applications are currently running and will not be shut down." 
-else
-echo "> kill -9 $CURRENT_PID" 
+echo "Execution started "
 
-sleep 10
 fi
-echo "/home/andrew/workspace/foodapp/src /var/www/html"
+echo "File moved from /home/andrew/workspace/foodapp/src /var/www/html"
 cp -r /home/andrew/workspace/foodapp/src /var/www/html
 
-
- echo "Execution complete."
+echo "Execution complete."
  
--- src/connection.php
+++ src/connection.php
@@ -1,7 +1,7 @@
 <?php
 $hostname="localhost";
-$user_name="root";
-$password= "";
+$user_name="andrew";
+$password= "Pass@1234";
 $db="dbfood";
 $con=mysqli_connect($hostname,$user_name,$password,$db);
 
